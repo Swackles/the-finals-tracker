ab7565367581cb48937156e7a62aedf055298f75
Swackles <Swackles@users.noreply.github.com>
2024-01-16 21:06:34 +0200

    feat: updated match history panel

--- src/pages/UserStatsV2/panels/matchHistoryPanel/MatchHistoryPanel.tsx
+++ src/pages/UserStatsV2/panels/matchHistoryPanel/MatchHistoryPanel.tsx
@@ -1,8 +1,62 @@
-import {TournamentCard} from "./TournamentCard";
+import Grid from '@mui/joy/Grid';
+import Chip from '@mui/joy/Chip';
+import Table from '@mui/joy/Table';
+import Typography from '@mui/joy/Typography';
 import {useGameStatsStore} from "@common/stores/gameStatsStore";
+import React, {useState} from "react";
+import {RoundCard} from "./RoundCard";
 
-export const MatchHistoryPanel = () => {
-    const { tournaments } = useGameStatsStore()
+const CARDS_IN_TOURNAMENT = 4
 
-    return tournaments.map(tournament => <TournamentCard key={tournament.id} data={tournament} />)
+const MatchHistoryPanel = () => {
+  const { tournaments } = useGameStatsStore()
+  const [expandedRow, setExpandedRow ] = useState<string[]>([])
+
+  return (
+    <Table>
+      <thead>
+      <tr>
+        <th>Map</th>
+        <th>Kills</th>
+        <th>Deaths</th>
+        <th>Revives</th>
+        <th>Damage</th>
+      </tr>
+      </thead>
+      <tbody>
+      {tournaments.map(tournament => (
+        <React.Fragment key={tournament.id}>
+          <tr onClick={() => setExpandedRow(prev => prev.includes(tournament.id) ? prev.filter(x => x !== tournament.id) : [tournament.id, ...prev])}>
+            <td style={{ borderBottom: "0 px" }}>
+              <Typography level="title-lg" color={tournament.won ? "success" : "primary" }>
+                {tournament.map}
+                <Chip sx={{ marginLeft: 1 }} color="neutral" size="sm" variant="soft">{tournament.startDateTime.toLocaleString()}</Chip>
+              </Typography>
+
+            </td>
+            <td>{tournament.rounds.reduce((a, b) => a + b.kills, 0)}</td>
+            <td>{tournament.rounds.reduce((a, b) => a + b.deaths, 0)}</td>
+            <td>{tournament.rounds.reduce((a, b) => a + b.revives, 0)}</td>
+            <td>{Math.round(tournament.rounds.reduce((a, b) => a + b.damage, 0))}</td>
+          </tr>
+          <tr style={{ visibility: expandedRow.includes(tournament.id) ? undefined : "collapse", transform: "height 200ms"}} >
+            <td colSpan={5}>
+              <Grid container
+                    direction="row"
+                    justifyContent="flex-start"
+                    alignItems="flex-start"
+                    flexWrap="wrap"
+                    spacing={2}>
+                {tournament.rounds.toReversed().map((round) => <RoundCard data={round} key={round.id}/>)}
+                {[...Array(CARDS_IN_TOURNAMENT - tournament.rounds.length)].map((_, i) => <RoundCard key={i}/>)}
+              </Grid>
+            </td>
+          </tr>
+        </React.Fragment>
+      ))}
+      </tbody>
+    </Table>
+  )
 }
+
+export default MatchHistoryPanel
--- src/pages/UserStatsV2/panels/matchHistoryPanel/RoundCard.tsx
+++ src/pages/UserStatsV2/panels/matchHistoryPanel/RoundCard.tsx
@@ -1,4 +1,12 @@
-import {AspectRatio, Card, CardContent, CardOverflow, Divider, Skeleton, Stack, Typography} from "@mui/joy";
+import AspectRatio from "@mui/joy/AspectRatio";
+import Card from "@mui/joy/Card";
+import CardContent from "@mui/joy/CardContent";
+import CardOverflow from "@mui/joy/CardOverflow";
+import Divider from "@mui/joy/Divider";
+import Grid from "@mui/joy/Grid";
+import Skeleton from "@mui/joy/Skeleton";
+import Stack from "@mui/joy/Stack";
+import Typography from "@mui/joy/Typography";
 import React from "react";
 import {RoundCardText} from "./RoundCardText"
 import {getMapVariant} from "@common/util";
@@ -12,18 +20,19 @@ export const RoundCard = ({ data }: RoundCardProps) => {
   const isLoading = !data
 
   return (
-    <Card variant="outlined" sx={{mt: 3, width: 350}}>
-      <CardOverflow>
-        <AspectRatio ratio="16/9">
-          <Skeleton animation={false} loading={isLoading}>
-            <img
-              src={data ? `/images/maps/${data.mapVariant}.png` : ""}
-              loading="lazy"
-              alt={data?.mapVariant || "loading..."}
-            />
-          </Skeleton>
-        </AspectRatio>
-        {data &&
+    <Grid xs={12} sm={6} xl={3}>
+      <Card variant="outlined">
+        <CardOverflow>
+          <AspectRatio ratio="16/9">
+            <Skeleton animation={false} loading={isLoading}>
+              <img
+                src={data ? `/images/maps/${data.mapVariant}.png` : ""}
+                loading="lazy"
+                alt={data?.mapVariant || "loading..."}
+              />
+            </Skeleton>
+          </AspectRatio>
+          {data &&
             <Typography style={{
               width: "100%",
               top: "10px",
@@ -31,33 +40,34 @@ export const RoundCard = ({ data }: RoundCardProps) => {
               textShadow: "1px 1px 5px white",
               position: "absolute",
               textAlign: "center"
-            }}  level="h4" color={data.won ? "success" : "danger"}>{getMapVariant(data.mapVariant)}</Typography>
-        }
-        <Divider inset="context" />
-        <CardContent orientation="horizontal">
-          <Stack sx={{ width: "100%" }}
-                 direction="row"
-                 alignItems="center"
-                 divider={<Divider orientation="vertical" />}
-                 justifyContent="space-around" >
-            <RoundCardText isLoading={isLoading}
-                           firstRow={data ? Math.round(data.kills / data.deaths * 100) / 100 : 0}
-                           secondRowRow="k/d" />
-            <RoundCardText isLoading={isLoading}
-                           firstRow={data ? data.kills : 0}
-                           secondRowRow="kills" />
-            <RoundCardText isLoading={isLoading}
-                           firstRow={data ? data.deaths : 0}
-                           secondRowRow="deaths" />
-            <RoundCardText isLoading={isLoading}
-                           firstRow={data ? data.respawns : 0}
-                           secondRowRow="respawns" />
-            <RoundCardText isLoading={isLoading}
-                           firstRow={data ? data.revives : 0}
-                           secondRowRow="revives" />
-          </Stack>
-        </CardContent>
-      </CardOverflow>
-    </Card>
+            }}  level="h4" color={data.won ? "success" : "primary"}>{getMapVariant(data.mapVariant)}</Typography>
+          }
+          <Divider inset="context" />
+          <CardContent orientation="horizontal">
+            <Stack sx={{ width: "100%" }}
+                   direction="row"
+                   alignItems="center"
+                   divider={<Divider orientation="vertical" />}
+                   justifyContent="space-around" >
+              <RoundCardText isLoading={isLoading}
+                             firstRow={data ? Math.round(data.kills / data.deaths * 100) / 100 : 0}
+                             secondRowRow="k/d" />
+              <RoundCardText isLoading={isLoading}
+                             firstRow={data ? data.kills : 0}
+                             secondRowRow="kills" />
+              <RoundCardText isLoading={isLoading}
+                             firstRow={data ? data.deaths : 0}
+                             secondRowRow="deaths" />
+              <RoundCardText isLoading={isLoading}
+                             firstRow={data ? data.respawns : 0}
+                             secondRowRow="respawns" />
+              <RoundCardText isLoading={isLoading}
+                             firstRow={data ? data.revives : 0}
+                             secondRowRow="revives" />
+            </Stack>
+          </CardContent>
+        </CardOverflow>
+      </Card>
+    </Grid>
   )
 }
